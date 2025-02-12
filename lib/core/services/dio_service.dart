82277343069f3717b570528789d01f204215c79c
Wissam Salah Almsalati <94019804+WissamAlmsalati@users.noreply.github.com>
2025-02-12 12:17:37 +0200

    /

--- lib/core/services/dio_service.dart
+++ lib/core/services/dio_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class DioService {
   static final DioService _instance = DioService._internal();
@@ -12,9 +13,9 @@ class DioService {
     dio = Dio(
       BaseOptions(
         baseUrl:
-            'https://dionv2-csbtgbecbxcybxfg.italynorth-01.azurewebsites.net/api/', 
-        connectTimeout: const Duration(seconds: 2000), 
-        receiveTimeout: const Duration(seconds: 2000), 
+            'https://dionv2-csbtgbecbxcybxfg.italynorth-01.azurewebsites.net/api/',
+        connectTimeout: const Duration(seconds: 2000),
+        receiveTimeout: const Duration(seconds: 2000),
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json',
@@ -32,15 +33,31 @@ class DioService {
           print('Response: ${response.statusCode}');
           return handler.next(response);
         },
-        onError: (DioException e, handler) {
-          print('Error: ${e.message}');
-          return handler.next(e);
+        onError: (DioError error, ErrorInterceptorHandler handler) async {
+          print('Error: ${error.message}');
+
+          // Check if the error status code is 401 (Unauthorized)
+          if (error.response?.statusCode == 401) {
+            await _handleUnauthorized();
+          }
+          // Pass the error along
+          return handler.next(error);
         },
       ),
     );
   }
 
-  Future<Response> get(String endpoint,{Map<String, dynamic>? queryParams, required Map<String, String> queryParameters}) async {
+
+  Future<void> _handleUnauthorized() async {
+    const storage = FlutterSecureStorage();
+    await storage.deleteAll();
+    print('User logged out due to 401 Unauthorized error.');
+    
+  }
+
+  Future<Response> get(String endpoint,
+      {Map<String, dynamic>? queryParams,
+      required Map<String, String> queryParameters}) async {
     return await dio.get(endpoint, queryParameters: queryParams);
   }
 
